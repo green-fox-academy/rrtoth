639015e246c066d2d190b7b5c8b741f44bba3302
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 14:04:18 +0200

    5th exercise

---
+++ week-01/day-3/two-numbers.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let myNum1: number = 22;
+let myNum2: number = 13;
+
+console.log(myNum2 + myNum1);
+console.log(myNum1 - myNum2);
+console.log(myNum1 * myNum2);
+console.log(myNum1 / myNum2);
+console.log(myNum1 % myNum2);
