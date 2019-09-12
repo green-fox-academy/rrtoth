6de21b3ba960ea8ca3a78e0eba190aae6e0b4342
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 14:05:37 +0200

    4th exercise minor correction

--- week-01/day-3/introduce-yourself.ts
+++ week-01/day-3/introduce-yourself.ts
@@ -4,8 +4,8 @@
 
 let myName: string = "Reka Toth";
 let age: number = 22;
-let height: number = 1.62
+let height: number = 1.62;
 
-console.log("My name is " + myName + ".")
+console.log("My name is " + myName + ".");
 console.log("I\'m " + age + " years old.");
 console.log("And I\'m " + height + "m tall.");
