e7366ffa7405adb52e4476e2b304ac1e6412786e
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 12:34:32 +0200

    4th exercise

---
+++ week-01/day-3/introduce-yourself.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+// I know the exercise was sth different, I just wanted to try out if this works
+
+let myName: string = "Reka Toth";
+let age: number = 22;
+let height: number = 1.62
+
+console.log("My name is " + myName + ".")
+console.log("I\'m " + age + " years old.");
+console.log("And I\'m " + height + "m tall.");
