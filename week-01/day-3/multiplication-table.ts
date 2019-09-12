ef76116de3309b729d393ab41d91bbe73ab2ec9d
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 22:11:07 +0200

    17th exercise

---
+++ week-01/day-3/multiplication-table.ts
@@ -0,0 +1,12 @@
+'use strict'
+
+let number: number = 15;
+let multiply: number = 1;
+
+while (multiply <= 10) {
+	console.log(multiply + " * " +  number + " = " + number * multiply);
+	multiply++;
+}
+
+//should I use while or for? only time will tell..
+
