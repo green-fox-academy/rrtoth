388ef6561f7450966023dcec95f35a1cffb9bf5a
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 23:08:47 +0200

    19th exercise

---
+++ week-01/day-3/fizz-buzz.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+//these exercises are getting more and more annoying
+
+let buzz: number = 1;
+
+while (buzz <= 100) {
+	if (buzz % 3 == 0 && buzz % 5 == 0) {
+		console.log("FizzBuzz");
+	} else if (buzz % 3 == 0) {
+		console.log("Fizz");
+	} else if (buzz % 5 == 0) {
+		console.log("Buzz");
+	} else {
+		console.log(buzz);
+	}
+	buzz++;
+}
+
