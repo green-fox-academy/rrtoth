b7f8c034fcf1602412332b47ca32499392d16bf2
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 15:32:26 +0200

    10th exercise

---
+++ week-01/day-3/define-basic-info.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let myName: string = "Reka Toth";
+let myAge: number =  22;
+let myHeight: number = 1.62;
+let married: boolean = false;
+
+console.log("My name is " + myName + ".", '\n', "I\'m " + myAge + " years old, " + myHeight + " tall, and whether I'm married?", '\n', "That's " + married);
+
+//trying to think about sentences that actually make sence was the hardest part 
