216df8526478218d20b2ea4b7a7ac3d60863619c
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-12 21:26:46 +0200

    16th exercise

---
+++ week-01/day-3/print-even.ts
@@ -0,0 +1,12 @@
+'use strict'
+
+let even: number = 0;
+while (even <= 500) {
+	console.log(even);
+	even++;
+	even++;
+}
+
+// I have no idea if this shoud be done with while or for, but it works, so I guess it's okay like this
+// also are 0 and 500 included or not?
+// also I\'m pretty sure this is not how this should be done, but again, it works, so I'm not gonna change it
