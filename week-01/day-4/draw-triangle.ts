5625732992b32a07d711e6028a7d8de96143018f
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-13 12:32:27 +0200

    20th exercise

---
+++ week-01/day-4/draw-triangle.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+let lineCount: number = 4;
+let star: string = "*";
+let stars: string = ""; 
+
+for(let a = 1; a <= lineCount; a++) {
+	stars = star + stars;
+	console.log(stars);
+}
+
