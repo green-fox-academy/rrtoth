a24d3d858c4057bfa839a9893479b27e47e02bb1
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-13 13:21:10 +0200

    22nd exercise

---
+++ week-01/day-4/draw-diamond.ts
@@ -0,0 +1,32 @@
+'use strict'
+
+let lineCount: number = 7;
+let lnC1: number = lineCount / 2 - .5;
+let lnC2: number = lineCount / 2 + .5;
+let star: string = "*";
+let stars: string = "*"; 
+let space: string = " ";
+let spaces: string = "";
+
+
+for(let a = 0; a < lnC2; a++) {
+	spaces = "";
+	for (let b = lnC2 - a; b > 0; b--) {
+		spaces += space;
+	}
+	console.log(spaces + stars);
+	stars += (star + star);
+}
+
+spaces = "  "; 
+
+for(let a = 1; a <= lnC1; a++) {
+        stars = "*";
+	for (let b = lnC1 - a; b > 0; b--) {
+		stars += (star + star);
+	}
+	console.log(spaces + stars);
+	spaces += space;
+}
+
+	
