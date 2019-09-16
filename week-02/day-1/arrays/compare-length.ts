1c524c61884076f029d3879dd03f3497ad1c08bb
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-16 11:18:59 +0200

    2nd exercise

---
+++ week-02/day-1/arrays/compare-length.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [3, 5,];
+
+if (firstList.length < secondList.length) {
+	console.log("p2 is longer");
+} else {
+	console.log("p1 is longer");
+}
+
+
