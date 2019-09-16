20279f22b561083afa165f6179751ce331d33349
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-16 13:19:37 +0200

    10th exercise

---
+++ week-02/day-1/arrays/colors.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let colors = [["lime", "forest green", "olive", "pale green", "spring green"],["orange red", "red", "tomato"],["orchid", "violet", "pink", "hot pink"]];
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
