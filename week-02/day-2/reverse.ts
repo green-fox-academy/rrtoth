8f11e65728cc47250b1d4a91b1786425de694203
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-17 12:49:46 +0200

    2nd exercise

---
+++ week-02/day-2/reverse.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+
+function reverse(a: string){
+	return a.split("").reverse().join("");
+}
+
+console.log(reverse(reversed));
+
+
