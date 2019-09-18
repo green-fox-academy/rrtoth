a7c23755193a4cbe9d0dea3b53f2af816ea8c2e5
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-18 15:07:40 +0200

    9th exercise

---
+++ week-02/day-3/center-box-function/center-box-function-ts.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// Create a function that draws one square and takes 1 parameters:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+let size: number = 300;
+let colors = ["black", "magenta", "indigo"];
+let x: number = 0; 
+
+for (let i = 0; i < 3; i++) {    
+    ctx.fillStyle = colors[i];
+    ctx.fillRect(canvas.width / 2 - size / 2, canvas.height / 2 - size / 2, size, size);
+    size = size - 50;
+};
+
