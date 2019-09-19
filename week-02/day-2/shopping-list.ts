c8c91acbbdd796eec730a9a336190fb4fb6eb100
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-19 14:22:27 +0200

    13th exercise

---
+++ week-02/day-2/shopping-list.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+let list: string[] = ["eggs", "milk", "fish", "apples", "bread", "chicken"];
+
+if (list.indexOf("milk") != -1) {
+    console.log("We have milk on the list")
+} else {
+    console.log("We don't have milk on the list")
+};
+
+if (list.indexOf("bananas") != -1) {
+    console.log("We have bananas on the list")
+} else {
+    console.log("We don't have bananas on the list")
+};
