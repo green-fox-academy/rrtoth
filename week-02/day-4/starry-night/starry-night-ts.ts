399295e2fddb0d7e96f00a43c70dbe56d03de7d6
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-19 11:38:52 +0200

    16th exercise

--- week-02/day-4/starry-night/starry-night-ts.ts
+++ week-02/day-4/starry-night/starry-night-ts.ts
@@ -10,11 +10,12 @@ const ctx = canvas.getContext('2d');
 //  - The stars should have random color (some shade of grey)
 
 function randomGrey() {
-    let a = 255*Math.random();
+    let a = Math.random() * 255;
     return 'rgb(' + a + ',' + a + ',' + a + ')';
 };
 
 for(let i = 0; i < 500; i++) {
+    let e: number = Math.random() * 3;
     ctx.fillStyle = randomGrey();
-    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 2, 2);
+    ctx.fillRect(Math.random() * canvas.width, Math.random() * canvas.height, e, e);
 };
