da1140da874011eb0fcf0f772524ec41a87c956f
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-20 17:33:04 +0200

    hexagon filled with color

--- week-02/day-5/super-hexagon/super-hexagon.ts
+++ week-02/day-5/super-hexagon/super-hexagon.ts
@@ -3,15 +3,16 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let change: number = 20;
+const change: number = 20;
+const half: number = Math.sqrt(change ** 2 - (change / 2) ** 2);
 let x: number = (canvas.width - change) / 2;
 let y: number = change;
 let l: number = 3;
 
 function randomColor() {
-    let a = Math.random() * 200;
-    let b = Math.random() * 200;
-    let c = Math.random() * 200;
+    let a = Math.random() * 255;
+    let b = Math.random() * 255;
+    let c = Math.random() * 255;
     return 'rgb(' + a + ',' + b + ',' + c + ')';
 };
 
@@ -19,33 +20,24 @@ function hexagon(x, y) {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + change, y);
-    ctx.stroke();
-    ctx.moveTo(x + change, y);
-    ctx.lineTo(x + change * 3 / 2, y + Math.sqrt(change ** 2 - (change / 2) ** 2));
-    ctx.stroke();
-    ctx.moveTo(x + change * 3 / 2, y + Math.sqrt(change ** 2 - (change / 2) ** 2));
-    ctx.lineTo(x + change, y + Math.sqrt(change ** 2 - (change / 2) ** 2) * 2);
-    ctx.stroke();
-    ctx.moveTo(x + change, y + Math.sqrt(change ** 2 - (change / 2) ** 2) * 2);
-    ctx.lineTo(x, y + Math.sqrt(change ** 2 - (change / 2) ** 2) * 2);
-    ctx.stroke();
-    ctx.moveTo(x, y + Math.sqrt(change ** 2 - (change / 2) ** 2) * 2);
-    ctx.lineTo(x - change / 2, y + Math.sqrt(change ** 2 - (change / 2) ** 2));
-    ctx.stroke();
-    ctx.moveTo(x - change / 2, y + Math.sqrt(change ** 2 - (change / 2) ** 2));
+    ctx.lineTo(x + change * 3 / 2, y + half);
+    ctx.lineTo(x + change, y + half * 2);
+    ctx.lineTo(x, y + half * 2);
+    ctx.lineTo(x - change / 2, y + half);
     ctx.lineTo(x, y);
+    ctx.fill();
     ctx.stroke();
 };
 
 for (let j = 0; j < 4; j++) {
     x = (canvas.width - change) / 2 + change * 1.5 * j;
-    y = change + Math.sqrt(change ** 2 - (change / 2) ** 2) * j;
+    y = change + half * j;
     l++;
     for (let i = 0; i < l; i++) {
-        ctx.strokeStyle = randomColor();
+        ctx.fillStyle = randomColor();
         hexagon(x, y);
         x -= change * 3 / 2;
-        y += Math.sqrt(change ** 2 - (change / 2) ** 2);
+        y += half;
     }
 };
 
@@ -53,14 +45,12 @@ l = 5;
 
 for (let j = 0; j < 3; j++) {
     x = (canvas.width - change) / 2 + change * 6;
-    y = change + 4 * Math.sqrt(change ** 2 - (change / 2) ** 2) + (Math.sqrt(change ** 2 - (change / 2) ** 2)) * 2 * j;
+    y = change + 4 * half + (half) * 2 * j;
     for (let i = 0; i <= l; i++) {
         x -= change * 3 / 2;
-        y += Math.sqrt(change ** 2 - (change / 2) ** 2);
-        ctx.strokeStyle = randomColor();
+        y += half;
+        ctx.fillStyle = randomColor();
         hexagon(x, y);
     }
     l--;
 };
-
-hexagon(x, y)
