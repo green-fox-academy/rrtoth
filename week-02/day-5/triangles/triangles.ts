02dcdcbcbb6d4658e53f8f94de6820830d1a1cc1
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-20 17:38:01 +0200

    triangles minor correction + color

--- week-02/day-5/triangles/triangles.ts
+++ week-02/day-5/triangles/triangles.ts
@@ -3,15 +3,17 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let change: number = 18;
+const change: number = 18;
+const m: number = Math.sqrt((change) ** 2 - (change / 2) ** 2);
 let x: number = (canvas.width - change) / 2;
 let y: number = change;
 let lines: number = 21;
 
+
 function randomColor() {
-    let a = Math.random() * 200;
-    let b = Math.random() * 200;
-    let c = Math.random() * 200;
+    let a = Math.random() * 255;
+    let b = Math.random() * 255;
+    let c = Math.random() * 255;
     return 'rgb(' + a + ',' + b + ',' + c + ')';
 };
 
@@ -19,23 +21,21 @@ function triangle(x, y) {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + change, y);
-    ctx.stroke();
-    ctx.moveTo(x + change, y);
-    ctx.lineTo(x + change / 2, y - Math.sqrt((change) ** 2 - (change / 2) ** 2));
-    ctx.stroke();
-    ctx.moveTo(x + change / 2, y - Math.sqrt((change) ** 2 - (change / 2) ** 2));
+    ctx.lineTo(x + change / 2, y - m);
     ctx.lineTo(x, y);
-    ctx.stroke();
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke()
 };
 
 for (let j = 1; j <= 21; j++) {
     for (let i = 0; i < lines; i++) {
-        ctx.strokeStyle = randomColor();
+        ctx.fillStyle = randomColor();
         triangle(x, y);
         x -= change / 2;
-        y += Math.sqrt((change) ** 2 - (change / 2) ** 2);
+        y += m;
     };
     lines--;
-    y = change + (Math.sqrt((change) ** 2 - (change / 2) ** 2)) * (j);
+    y = change + m * j;
     x = (canvas.width - change) / 2 + (change / 2) * j;
 };
