8e253f12e6dd07959bbd316206ac42d5a43dcffa
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-24 13:55:29 +0200

    2nd exercise

---
+++ week-03/day-1/print-each-line.ts
@@ -0,0 +1,20 @@
+// Write a program that opens a file called "my-file.txt", then prints
+// each line from the file.
+// If the program is unable to read the file (for example it does not exist),
+// then it should print the following error message: "Unable to read file: my-file.txt"
+'use strict';
+
+declare function require(path: string): any;
+    export {};
+
+const fs = require('fs');
+
+let file: string = "";
+
+try {
+    file = fs.readFileSync('my-file.txt', 'utf8')
+    console.log(file);
+}
+catch (error) {
+    console.log("Unable to read file: my-file.txt");
+}
