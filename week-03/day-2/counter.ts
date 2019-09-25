fc61386c9e4e8a187324bd267f32c4a9b9e6f0a8
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-25 14:40:29 +0200

    5th exercise

---
+++ week-03/day-2/counter.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+class Counter {
+    integer: number = 0;
+
+    constructor(integer: number = 0) {
+        this.integer = integer;
+    }
+    public add(number?: number) {
+        if (number) {
+            this.integer += number;
+        } else {
+            this.integer++;
+        }
+    }
+
+    public get() {
+        return this.integer;
+    }
+
+    public reset() {
+        this.integer = 0;
+    }
+}
+
+let one = new Counter();
+
+one.add();
+one.reset();
+one.add(5);
+
+console.log(one.get())
