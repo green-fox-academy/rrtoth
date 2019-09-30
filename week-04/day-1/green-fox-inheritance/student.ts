d47467d2b1f6b564b24d48a5aff5ff96926c2b5d
rekatoth <reka.rebeka.toth@gmail.com>
2019-09-30 13:59:22 +0200

    Green Fox Inheritance exercise/2

---
+++ week-04/day-1/green-fox-inheritance/student.ts
@@ -0,0 +1,32 @@
+import { Person } from "./person"
+
+export class Student extends Person {
+    protected _previousOrgaization: string;
+    protected _skippedDays: number;
+
+    constructor(name?: string, age?: number, gender?: string, previousOrg: string = "The School of Life", skipped: number = 0) {
+        super(name, age, gender);
+        this._previousOrgaization = previousOrg;
+        this._skippedDays = skipped;
+    }
+
+    getGoal() {
+        console.log("My goal is: Be a junior software developer.");
+    }
+
+    introduce() {
+        console.log("Hi, I'm " + this._name + ", a " + this._age + " year old "
+            + this._gender + " from " + this._previousOrgaization + " who skipped "
+            + this._skippedDays + " days from the course already.");
+    }
+
+    skipDays(numberOfDays?){
+        if (numberOfDays){
+            this._skippedDays += numberOfDays;
+        } else{
+            this._skippedDays++;
+        }
+    }
+}
+
+let student1 = new Student;
