31c9950ed42300f203d3a16f89f1a7bac60bea76
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-01 13:41:54 +0200

    Flyable/2

---
+++ week-04/day-2/flyable/helicopter.ts
@@ -0,0 +1,20 @@
+import { Vehicle } from "./flyable"
+import { Flyable } from "./flyable"
+
+export class Helicopter extends Vehicle implements Flyable {
+    constructor(doors: number = 4, speed?: number, seats: number = 6, type?: string) {
+        super(doors, speed, seats, type)
+    }
+
+    takeOff(): void{
+        console.log("takeoff test")
+    }
+    
+    fly(): void{
+        console.log("fly test")
+    }
+
+    land(): void{
+        console.log("land test")
+    }
+}
