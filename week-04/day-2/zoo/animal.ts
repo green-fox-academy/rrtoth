8e648d8193b4fafcfb6dd667f3831d0422fc2746
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-01 12:17:18 +0200

    zoo/1

---
+++ week-04/day-2/zoo/animal.ts
@@ -0,0 +1,21 @@
+export abstract class Animal {
+    name: string;
+    age: number;
+    gender: string;
+    numOfLegs: number;
+    integument: string;
+
+    constructor(name: string, age: number, gender: string, legs: number, integument: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.numOfLegs = legs;
+        this.integument = integument;
+    }
+
+    getName() {
+        return this.name
+    }
+
+    breed() { }
+}
