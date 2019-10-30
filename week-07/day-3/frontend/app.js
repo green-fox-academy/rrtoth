e0146080be3aee7db53e46b1283e60a8c805174d
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-30 14:24:05 +0100

    doubling

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -9,9 +9,25 @@ const PORT = 8080;
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling/', (req, res) => {
+    let double = {}
+
+    if (req.query.input) {
+        let input = req.query.input;
+
+        double["received"] = input;
+        double["result"] = input * 2;
+    } else {
+        double["error"] = "Please provide an input!"
+    }
+
+    res.send(double)
+});
+
+
 app.listen(PORT, () => {
-  console.log(`The server is up and running on ${PORT}`);
+    console.log(`The server is up and running on ${PORT}`);
 });
