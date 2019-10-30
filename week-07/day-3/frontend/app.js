74bd82f0987ab5e87c89c0327932d8d9938ad31a
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-30 12:24:56 +0100

    folder structure, created necessary files, folders

---
+++ week-07/day-3/frontend/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on ${PORT}`);
+});
