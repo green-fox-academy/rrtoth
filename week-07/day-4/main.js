e74b2fec88518d05cd7ca9059895f47b2cd56f61
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-31 16:20:51 +0100

    post, upvote, downvote added, but doesn't work

--- week-07/day-4/main.js
+++ week-07/day-4/main.js
@@ -6,7 +6,7 @@ const env = require('dotenv').config();
 
 app.use(express.json());
 
-let conn = mysql.createConnection ({
+let conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -19,21 +19,63 @@ conn.connect((err) => {
 });
 
 //gets date 10/31/2019 12:24:18 PM format
-const printCurrentDate = (timestamp) => {
-    let date = new Date(timestamp);
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// const printCurrentDate = (timestamp) => {
+//     let date = new Date(timestamp);
+//     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+//     let daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${daysOfTheWeek[date.getDay()]} ${date.toLocaleTimeString()}`
-}
+//     return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${daysOfTheWeek[date.getDay()]} ${date.toLocaleTimeString()}`
+// }
 
-app.get('/', function (req, res) {
-    conn.query(`SELECT * FROM posts;`, (err, rows) => {
+app.get('/posts', (req, res) => {
+    conn.query('SELECT * FROM posts;', (err, rows) => {
         if (err) {
             console.log(err.toString());
         }
         res.send(rows);
     });
 });
+
+app.post('/posts/:post', (req, res) => {
+    let input = req.body.input
+    let date = new Date(timestamp)
+    let post = `INSERT INTO posts(title, url, timestamp, score, oID) VALUES (${input.title}, ${input.url}, ${date}, 0, 0);`
+
+    conn.query(post, (err, result) => {
+        if (err) throw err;
+    })
+})
+
+app.post('/posts/:id/:action', (req, res) => {
+    let action = req.params.action
+    let id = parseInt(req.params.id)
+
+    if (action) {
+        let score
+
+        conn.query(`SELECT pId FROM posts WHERE pId = ${id}`, (err, result) => {
+            if (err) throw err;
+            score = result.pId;
+            res.send('ok')
+        })
+
+        if (action == 'upvote') {
+            conn.query(`UPDATE posts SET score = ${score + 1} WHERE pId = ${id}`, (err, result) => {
+                if (err) throw err;
+                // conn.query('SELECT * FROM posts', (err, result) => {
+                //     if (err) throw err;
+                //     res.send(result)
+                // })
+                res.send('success')
+            })
+        } else if (action == 'downvote') {
+            conn.query(`UPDATE posts SET score = ${score - 1} WHERE pId = ${id}`, (err, result) => {
+                res.send('success')
+            })
+        } else {
+            res.send({ error: 'wrong action' })
+        }
+    }
+})
 
 app.listen(PORT);
