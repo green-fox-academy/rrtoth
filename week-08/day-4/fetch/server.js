17e79aa782842ddc64046cd45f35888a4f729518
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-07 16:35:53 +0100

    fetch some joke exercise complete

---
+++ week-08/day-4/fetch/server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendfile('index.html'));
+
+app.listen(port)
