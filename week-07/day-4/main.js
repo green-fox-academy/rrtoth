c72d95717ea130df2175967bba2297edb3539379
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-08 15:22:04 +0100

    reddit backend added multipleStatements: true & minor changes

--- week-07/day-4/main.js
+++ week-07/day-4/main.js
@@ -10,11 +10,12 @@ let conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    multipleStatements: true
 });
 
 conn.connect((err) => {
-    err ? console.log(err) : console.log('Connected!');
+    err ? (console.log(err), res.status(500)) : console.log('Connected!');
 });
 
 //hi
@@ -25,9 +26,7 @@ app.get('/hello', (req, res) => {
 //see all posts
 app.get('/posts', (req, res) => {
     conn.query('SELECT * FROM posts;', (err, rows) => {
-        if (err) throw err;
-        res.set('Content-Type: application/json');
-        res.send(rows);
+        err ? (console.log(err), res.status(500)) : (res.set('Content-Type: application/json'), res.send(rows));
     });
 });
 
@@ -36,7 +35,7 @@ app.get('/post/:id', (req, res) => {
     let id = req.params.id;
 
     conn.query(`SELECT * FROM posts WHERE pId = ${id}`, (err, row) => {
-        err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row))
+        err ? (console.log(err), res.status(500)) : (res.set('Content-Type: application/json'), res.send(row));
     })
 })
 
@@ -46,31 +45,34 @@ app.post('/posts', (req, res) => {
     let url = req.body.url;
     let date = Math.floor(Date.now() / 1000);
 
-    let post = `INSERT INTO posts (title, url, timestamp, score, oID) VALUES ("${title}", "${url}", ${date}, 0, 0);`;
+    let post = `INSERT INTO posts (title, url, timestamp, score, oID) 
+                    VALUES ("${title}", "${url}", ${date}, 0, 0);
+                SELECT * FROM posts 
+                    ORDER BY pId DESC 
+                    LIMIT 1;`;
 
-    conn.query(post, (err, result) => {
-        if (err) throw err;
-
-        conn.query('SELECT * FROM posts ORDER BY pId DESC LIMIT 1;', (err, row) => {
-            err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row));
-        })
+    conn.query(post, (err, row) => {
+        err ? (console.log(err), res.status(500)) : (res.set('Content-Type: application/json'), res.send(row[1]));
     })
 })
 
 //up- and downvote posts
 app.put('/posts/:id/:action', (req, res) => {
     let id = req.params.id;
-    let action = req.params.action
+    let action = req.params.action;
 
     if (action == 'upvote' || action == 'downvote') {
-        action == 'upvote' ? post = `UPDATE posts SET score = score + 1 WHERE pId = ${id};` : post = `UPDATE posts SET score = score - 1 WHERE pId = ${id};`;
+        let newScore = "";
+        action == 'upvote' ? newScore = 'score + 1' : newScore = 'score - 1';
 
-        conn.query(post, (err, result) => {
-            if (err) throw err;
+        let post = `UPDATE posts 
+                        SET score = ${newScore}
+                        WHERE pId = ${id};
+                    SELECT * FROM posts 
+                        WHERE pId = ${id};`
 
-            conn.query('SELECT * FROM posts ORDER BY pId DESC LIMIT 1;', (err, row) => {
-                err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row));
-            })
+        conn.query(post, (err, row) => {
+            err ? (console.log(err), res.status(500)) : (res.set('Content-Type: application/json'), res.send(row[1]));
         })
     } else {
         res.send('wrong action')
