ead23904ab6ed332d61abc905ff4492cdcd6d2ba
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-08 14:18:57 +0100

    reddit backend basics now work

--- week-07/day-4/main.js
+++ week-07/day-4/main.js
@@ -10,88 +10,70 @@ let conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 conn.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
+    err ? console.log(err) : console.log('Connected!');
 });
 
+//hi
 app.get('/hello', (req, res) => {
     res.send("Hello World!");
 });
 
+//see all posts
 app.get('/posts', (req, res) => {
-    conn.query('SELECT * FROM posts\
-                LEFT JOIN users\
-                    ON users.pID;', (err, rows) => {
-        if (err) {
-            console.log(err.toString());
-        }
+    conn.query('SELECT * FROM posts;', (err, rows) => {
+        if (err) throw err;
+        res.set('Content-Type: application/json');
         res.send(rows);
     });
 });
 
-app.post('/posts/:post', (req, res) => {
-    let input = req.body.input
-    let date = new Date(timestamp)
-    let post = `INSERT INTO posts(title, url, timestamp, score, oID) VALUES (${input.title}, ${input.url}, ${date}, 0, 0);`
+//get a specific bost by ID
+app.get('/post/:id', (req, res) => {
+    let id = req.params.id;
 
-    conn.query(post, (err, result) => {
-        if (err) {
-            console.log(err.toString());
-        }
-        res.send('ok')
+    conn.query(`SELECT * FROM posts WHERE pId = ${id}`, (err, row) => {
+        err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row))
     })
 })
 
-app.post('/posts/:id/:action', (req, res) => {
-    let id = parseInt(req.params.id.toString())
+//post a new post
+app.post('/posts', (req, res) => {
+    let title = req.body.title;
+    let url = req.body.url;
+    let date = Math.floor(Date.now() / 1000);
 
-    if (req.params.action) {
-        let action = req.params.action
+    let post = `INSERT INTO posts (title, url, timestamp, score, oID) VALUES ("${title}", "${url}", ${date}, 0, 0);`;
 
-        conn.query(`SELECT * FROM posts WHERE pId = ${id}`, (err, result) => {
-            if (err) {
-                console.log(err);
-            }
+    conn.query(post, (err, result) => {
+        if (err) throw err;
 
-            let score = parseInt((result[0].pId).toString())
-            console.log(score)
-            if (action == 'upvote' || action == 'downvote') {
-                if (action == 'upvote') {
-                    conn.query(`UPDATE posts SET score = ${score + 1} WHERE pId = ${id}`, (err, result) => {
-                        if (err) {
-                            console.log(err.toString());
-                        }
-                        conn.query('SELECT * FROM posts', (err, result) => {
-                            if (err) throw err;
-                            res.send(result)
-                        })
-                    })
-                } else if (action == 'downvote') {
-                    conn.query(`UPDATE posts SET score = ${score - 1} WHERE pId = ${id}`, (err, result) => {
-                        if (err) {
-                            console.log(err.toString());
-                        }
-                        conn.query('SELECT * FROM posts', (err, result) => {
-                            if (err) throw err;
-                            res.send(result)
-                        })
-                    })
-                }
-                // conn.query(`SELECT * FROM posts WHERE pId = ${id}`, (err, result) => {
-                //     if (err) {
-                //         console.log(err);
-                //     }
-                //     let vote = parseInt((result[0].vote).toString())
-                //     conn.query(`UPDATE users SET vote = ${vote + 1} WHERE oID`)
-                // })
-            } else {
-                res.send('wrong action')
-            }
+        conn.query('SELECT * FROM posts ORDER BY pId DESC LIMIT 1;', (err, row) => {
+            err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row));
         })
+    })
+})
+
+//up- and downvote posts
+app.put('/posts/:id/:action', (req, res) => {
+    let id = req.params.id;
+    let action = req.params.action
+
+    if (action == 'upvote' || action == 'downvote') {
+        action == 'upvote' ? post = `UPDATE posts SET score = score + 1 WHERE pId = ${id};` : post = `UPDATE posts SET score = score - 1 WHERE pId = ${id};`;
+
+        conn.query(post, (err, result) => {
+            if (err) throw err;
+
+            conn.query('SELECT * FROM posts ORDER BY pId DESC LIMIT 1;', (err, row) => {
+                err ? console.log(err) : (res.set('Content-Type: application/json'), res.send(row));
+            })
+        })
+    } else {
+        res.send('wrong action')
     }
 })
 
