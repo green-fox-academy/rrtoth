e5e251d572dab51550d1d360a8e894966ad61537
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-12 14:50:07 +0100

    greeter - tests 1-4

--- week-07/day-3/frontend/test/test.js
+++ week-07/day-3/frontend/test/test.js
@@ -12,6 +12,7 @@ test('doubling test #1', (t) => {
         .expect(200)
         .end((err, res) => {
             let doublingResponse = { 'received': '10', 'result': 20 }
+
             t.error(err, 'no err')
             t.deepEqual(res.body, doublingResponse, 'correct response')
             t.end()
@@ -25,8 +26,68 @@ test('doubling test #2', (t) => {
         .expect(400)
         .end((err, res) => {
             let doublingResponse = { "error": "Please provide an input!" }
+
             t.error(err, 'no err')
             t.deepEqual(res.body, doublingResponse, 'correct response')
+            t.end()
+        });
+})
+
+test('greeter test #1', (t) => {
+    request(app)
+        .get('/greeter')
+        .query({ name: 'asd', title: 'dsa' })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+            let greeterResponse = { welcome_message: 'Oh, hi there asd, my dear dsa!' };
+
+            t.error(err, 'no err')
+            t.deepEqual(res.body, greeterResponse, 'correct response')
+            t.end()
+        });
+})
+
+test('greeter test #2', (t) => {
+    request(app)
+        .get('/greeter')
+        .query({ name: 'asd' })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((err, res) => {
+            let greeterResponse = { error: 'Please provide a title!' };
+
+            t.error(err, 'no err')
+            t.deepEqual(res.body, greeterResponse, 'correct response')
+            t.end()
+        });
+})
+
+test('greeter test #3', (t) => {
+    request(app)
+        .get('/greeter')
+        .query({ title: 'dsa' })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((err, res) => {
+            let greeterResponse = { error: 'Please provide a name!' };
+
+            t.error(err, 'no err')
+            t.deepEqual(res.body, greeterResponse, 'correct response')
+            t.end()
+        });
+})
+
+test('greeter test #4', (t) => {
+    request(app)
+        .get('/greeter')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((err, res) => {
+            let greeterResponse = { error: 'Please provide a name and a title!' };
+
+            t.error(err, 'no err')
+            t.deepEqual(res.body, greeterResponse, 'correct response')
             t.end()
         });
 })
