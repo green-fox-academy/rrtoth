83a80310899fc9208fcacfce503a3011d66fcd6c
rekatoth <reka.rebeka.toth@gmail.com>
2019-11-12 13:06:51 +0100

    exercise 1 - test 2

--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -21,9 +21,22 @@ test('groot endpoint', (t) => {
         });
 });
 
+test('groot endpoint, no param', (t) => {
+    request(app)
+        .get('/groot/')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((err, res) => {
+            let errResponse = {
+                "error": "I am Groot!"
+            }
+
+            t.error(err, 'no err')
+            t.deepEqual(res.body, errResponse, 'correct response')
+            t.end()
+        })
+})
 
 // Write a test where you check:
-// With giving a parameter the status is ok,
-// and the given response is the same as expected
 // Without giving a parameter the status is not ok,
 // and the given error response is the same as expected
